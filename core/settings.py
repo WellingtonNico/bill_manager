6a013e7f18f0688eb5eef804a58c131ee60a211f
Wellington Adriano Nicoletti de Abreu <wellingtonicoletti@gmail.com>
2022-10-02 21:04:12 -0300

    adicionadas migrations dos usuários

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'users',
 ]
 
 ROOT_URLCONF = 'core.urls'
