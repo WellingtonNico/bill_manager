7669b02b453fbe704d2b4f5fb7220fb5e09fd26c
Wellington Adriano Nicoletti de Abreu <wellingtonicoletti@gmail.com>
2022-10-05 17:35:52 -0300

    adicionados templates de registração

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm,PasswordResetForm
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import HTML,Layout,Fieldset
 
@@ -12,9 +12,30 @@ class CustomAuthenticationForm(AuthenticationForm):
         ),
         HTML(
             '''
+            {% if form.errors %}
+                <a href="{% url 'password_reset' %}" class="my-1">Esqueci minha senha</a>
+            {% endif %}
             <div class="row mt-3 justify-content-center">
                 <div class="col text-center">
                     <button type="submit" class="btn btn-success"><i class="material-icons">login</i> login</button>
+                </div>
+            </div>
+            '''
+        )
+    )
+
+class CustomPasswordResetForm(PasswordResetForm):
+    helper = FormHelper()
+    helper.layout =Layout(
+        Fieldset(
+            'Recuperação de senha',
+            'email',
+        ),
+        HTML(
+            '''
+            <div class="row mt-3 justify-content-center">
+                <div class="col text-center">
+                    <button type="submit" class="btn btn-success"><i class="material-icons">done</i> enviar email</button>
                 </div>
             </div>
             '''
--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,8 @@
-"""core URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.urls import path, reverse_lazy
 from django.urls import include
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView,TemplateView
 from django.contrib.auth.views import (
     LogoutView,
     LoginView,
@@ -28,14 +14,28 @@ from django.contrib.auth.views import (
     PasswordResetCompleteView
 )
 
-from core.forms import CustomAuthenticationForm
+from core.forms import CustomAuthenticationForm, CustomPasswordResetForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/',LoginView.as_view(form_class=CustomAuthenticationForm),name='login'),
-    path('logout/',LogoutView.as_view(),name='logout'),
     path('bills/',include('bills.urls')),
     path('bill_categories/',include('bill_categories.urls')),
     path('bill_chargers/',include('bill_chargers.urls')),
     path('',RedirectView.as_view(url=reverse_lazy('bill_list'))),
+
+
+    path('login/',LoginView.as_view(form_class=CustomAuthenticationForm),name='login'),
+    path('logout/',LogoutView.as_view(),name='logout'),
+    # REGISTRAÇÃO e CONTROLE
+    path('login/',LoginView.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(),name='logout'),
+    # path('support',login_required(SupportView.as_view()),name='support'),
+    path('support_sent_confirmation',login_required(TemplateView.as_view(template_name='support/support_sent_confirmation.html')),name='support_sent_confirmation'),
+    path('support/tutorials',login_required(TemplateView.as_view(template_name='support/tutorials.html')),name='support_tutorials'),
+    path('password_change/', login_required(PasswordChangeView.as_view()), name='password_change'),
+    path('password_change/done/', login_required(PasswordChangeDoneView.as_view()), name='password_change_done'),
+    path('password_reset/done/',PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_reset/', PasswordResetView.as_view(html_email_template_name='registration/password_reset_email.html',form_class=CustomPasswordResetForm), name='password_reset'),
 ]
