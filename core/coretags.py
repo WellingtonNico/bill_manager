dcbeeee651ce94094f8f1a4eadd7e9dfddfec508
Wellington Adriano Nicoletti de Abreu <wellingtonicoletti@gmail.com>
2022-10-07 09:52:18 -0300

    adicionado template com renderização de páginas

--- core/coretags.py
+++ core/coretags.py
@@ -1,7 +1,20 @@
 from django import template
+from django.utils.safestring import mark_safe
+
 
 register = template.Library()
 
 @register.filter(name='to_int')
 def to_json(value):
     return int(value)
+
+
+@register.filter(name='build_params')
+def build_params(request):
+    newUrl = ''
+    for key,value in request.GET.items():
+        if key != 'page' and value:
+            newUrl += f'{key}={value}&'
+
+    return newUrl
+
